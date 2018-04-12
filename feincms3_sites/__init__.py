0020fae07eaa133d533c810cbcf2e267211ee377
Matthias Kestenholz <mk@feinheit.ch>
2018-04-12 11:44:17 +0200

    Nicer name for app

--- feincms3_sites/__init__.py
+++ feincms3_sites/__init__.py
@@ -1,2 +1,4 @@
 VERSION = (0, 0, 0)
 __version__ = '.'.join(map(str, VERSION))
+
+default_app_config = 'feincms3_sites.apps.SitesAppConfig'
---
+++ feincms3_sites/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.text import capfirst
+from django.utils.translation import ugettext_lazy as _
+
+
+class SitesAppConfig(AppConfig):
+    name = 'feincms3_sites'
+    verbose_name = capfirst(_('sites'))
