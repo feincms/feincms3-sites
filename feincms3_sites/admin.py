0ea940ca289252ea52a4e6445d2e0446bc268566
Matthias Kestenholz <mk@feinheit.ch>
2018-04-12 11:44:26 +0200

    Hide the host_re field a bit

--- feincms3_sites/admin.py
+++ feincms3_sites/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django.utils.text import capfirst
+from django.utils.translation import gettext_lazy as _
 
 from .models import Site
 
 
 @admin.register(Site)
 class SiteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': ('is_default', 'host'),
+        }),
+        (capfirst(_('advanced')), {
+            'fields': ('host_re',),
+            'classes': ('collapse',),
+        }),
+    ]
     list_display = ('host', 'is_default', 'host_re')
     ordering = ('-is_default',)
